21e3ea1b22fd68cca0872898aebc60220cb7e9b7
César Cancino <yo@cesarcancino.com>
2015-10-15 20:55:32 -0400

    maqueta5.php

---
+++ maqueta5.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="utf-8" />
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" />
+            <link type="text/css" rel="stylesheet" href="css/estilos.css" />
+            <script type="text/javascript" src="js/funciones.js"></script>
+        </head>
+        <body>
+            
+            <ul class="nav nav-tabs">
+              <li role="presentation"><a href="#" onclick="cambiarImagen('koala.jpg');">Koala</a></li>
+              <li role="presentation"><a href="#" onclick="cambiarImagen('pin.jpg');">Pingüino</a></li>
+              <li role="presentation"><a href="#" onclick="cambiarImagen('rojo.jpg');">Flores</a></li>
+              <li role="presentation"><a href="#" onclick="cambiarImagen('tulipanes.jpg');">Faro</a></li>
+            </ul>
+            <hr />
+            <div class="container">
+                <img src="images/koala.jpg" class="imagen" id="foto" />
+            </div>
+        </body>
+    </html>
+
+
